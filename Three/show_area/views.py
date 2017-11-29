0c709a9706a03276450b3367d4e50e991c93b645
LyCq <15233219796@163.com>
2017-11-29 21:53:23 +0800

    显示省份

--- Three/show_area/views.py
+++ Three/show_area/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from show_area.models import AreaInfo
 
 # Create your views here.
 def show_menu(request):
     """显示三级联动的菜单"""
     return render(request,'show_area/show_menu.html')
+
+
+def show_sheng(request):
+    """获取省的json数据"""
+    shengList = AreaInfo.objects.filter(parent__isnull=True)
+    new_list = []
+    for sheng in shengList:
+        new_list.append({'id':sheng.id,"name":sheng.name})
+
+    jsonData = {'jsonData':new_list}
+    return JsonResponse(jsonData)
