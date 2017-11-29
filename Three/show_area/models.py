46bec92dbfb8cd585c6e1dab1126d477c20e222d
LyCq <15233219796@163.com>
2017-11-29 21:09:05 +0800

    配置环境

--- Three/show_area/models.py
+++ Three/show_area/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class AreaInfo(models.Model):
+    name = models.CharField(max_length=30)
+    parent = models.ForeignKey('self',null=True,blank=True)
+
+    class Meta:
+        db_table= 'areas'
