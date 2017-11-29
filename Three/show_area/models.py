ca721b5781969e63f36af0b5c6f68c0a42f42ce6
LyCq <15233219796@163.com>
2017-11-29 21:18:07 +0800

    显示菜单

--- Three/show_area/models.py
+++ Three/show_area/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class AreaInfo(models.Model):
     name = models.CharField(max_length=30)
     parent = models.ForeignKey('self',null=True,blank=True)
--- Three/show_area/views.py
+++ Three/show_area/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_menu(request):
+    """显示三级联动的菜单"""
+    return render(request,'show_area/show_menu.html')
